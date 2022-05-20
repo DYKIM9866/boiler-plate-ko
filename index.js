894c692e1a1e0b78829c4e9133a0d70dde33242a
dykim <eodud9866@naver.com>
2022-05-20 15:34:23 +0900

    gitignore를 이용한 비밀정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const {User} = require("./models/User");
+
+//분석해서 가져오는 것
+//application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kdy950930:1q2w3e4r!@cluster0.lik3n.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log('MongoDB error: ',e))
 
 
-app.get('/',(req,res) => res.send('Hello World~~헬로우'))
+app.get('/',(req,res) => res.send('Hello World~~양'))
+
+app.post('/register',(req,res) => {
+
+    //회원가입 시 필요한 정보들을 client에서 가져오면
+    //데이터 베이스에 입력
+    const user = new User(req.body);
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({succcess: false,err});
+        return res.status(200).json({
+            success: true
+        });
+    }); //mongoDB메서드
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
